18706b5569ec82ca1c56e390b35951560d13b673
Marcell Schuh <marcell.schuh@gmail.com>
2017-10-25 16:41:40 +0200

    Initial Commit

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+adb install ./apks/elso.apk
+adb install ./apks/TokinGame.Dezso.Vadaszat.apk
+adb install ./apks/superpista.apk
+adb install ./apks/rewind.apk
+adb push ./apks/wall.jpg /sdcard/wall.jpg
